e4de28bb775e5250d98a07cd9115b5e6da92a52b
VictorHanert <vict660m@stud.kea.dk>
2025-04-25 12:43:54 +0200

    Assignemnt 11a

---
+++ 00._Assignments/11a._Auth_Integration/app.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import pkg from "express-openid-connect"
+import dotenv from 'dotenv';
+
+const { auth, requiresAuth } = pkg;
+
+dotenv.config();
+
+const app = express();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.AUTH0_SECRET,
+    baseURL: process.env.AUTH0_BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    authorizationParams: {
+      response_type: 'code',
+      response_mode: 'query',
+      scope: 'openid profile email',
+    }
+  };
+
+app.use(auth(config));
+
+// Public route
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Home OAUTH</h1>
+    ${req.oidc.isAuthenticated() ? `
+      <p>Welcome ${req.oidc.user.name}</p>
+      <a href="/profile">Profile</a> |
+      <a href="/logout">Logout</a>
+    ` : `<a href="/login">Login</a>`}
+  `);
+});
+
+// Protected route
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user, null, 2));
+  });
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log('Server running on port', PORT));
