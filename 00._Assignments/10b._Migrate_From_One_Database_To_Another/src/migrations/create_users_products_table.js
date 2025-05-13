f2d14cd2bf3f99a64c2645ebb2aac4e6c6a17c61
VictorHanert <vict660m@stud.kea.dk>
2025-05-13 21:37:29 +0200

    dbs

--- 00._Assignments/10b._Migrate_From_One_Database_To_Another/src/migrations/create_users_products_table.js
+++ 00._Assignments/10b._Migrate_From_One_Database_To_Another/src/migrations/create_users_products_table.js
@@ -1,13 +1,11 @@
 export function up(knex) {
-    return knex.schema
-        .createTable('users', (table) => {
-            table.increments('id');
+    return knex.schema.createTable('users', (table) => {
+            table.increments('id').unsigned().primary();
             table.string('first_name', 255).notNullable();
             table.string('last_name', 255).notNullable();
         });
 }
 
 export function down(knex) {
-    return knex.schema
-        .dropTable('users');
+      return knex.schema.dropTableIfExists('users');
 }
